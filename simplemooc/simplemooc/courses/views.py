0016c27419595ac68829a7004d6428ae2d5b907c
rafael <rafaelsevla@gmail.com>
2018-09-09 20:38:02 -0300

    call screen with slug and redirecionate in list courses to detailed page

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -10,10 +10,19 @@ def index(request):
 	}
 	return render(request, template_name, context)
 
-def details(request, pk):
-	courses = get_object_or_404(Course, pk=pk)
+# def details(request, pk):
+# 	courses = get_object_or_404(Course, pk=pk)
+# 	context = {
+# 		'course': courses
+# 	}
+# 	template_name = 'courses/details.html'
+# 	return render(request, template_name, context)
+
+def details(request, slug):
+	courses = get_object_or_404(Course, slug=slug)
 	context = {
 		'course': courses
 	}
 	template_name = 'courses/details.html'
 	return render(request, template_name, context)
+

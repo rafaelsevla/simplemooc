7e715581556621b3588a73b2e04b9545b8818d4a
rafael <rafaelsevla@gmail.com>
2018-09-09 19:42:15 -0300

    added field about in courses and create page details for him

--- simplemooc/simplemooc/courses/urls.py
+++ simplemooc/simplemooc/courses/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from simplemooc.courses.views import index
+from simplemooc.courses.views import index, details
 # from simplemooc.courses.views import home, contact
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^(?P<pk>\d+)/$', details, name='details'),
 ]
--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -9,3 +9,11 @@ def index(request):
 		'courses': courses
 	}
 	return render(request, template_name, context)
+
+def details(request, pk):
+	courses = Course.objects.get(pk=pk)
+	context = {
+		'course': courses
+	}
+	template_name = 'courses/details.html'
+	return render(request, template_name, context)

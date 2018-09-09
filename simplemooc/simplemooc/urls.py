46ac8c10791659c13a1bc2b0c6099c0716f597ce
rafael <rafaelsevla@gmail.com>
2018-09-09 02:35:44 -0300

    added namespace to organize the code in urls

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
-from simplemooc.core.views import home, contact
 
 urlpatterns = [
+    url(r'^', include(('simplemooc.core.urls','core'), namespace='core')),
     path('admin/', admin.site.urls),
-    url(r'^$', home, name='home'),
-    url(r'^contato/$', contact, name='contact'),
 ]

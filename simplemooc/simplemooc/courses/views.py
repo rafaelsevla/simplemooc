5ceddb00f2cc0f2971b255f74ac1b4fe6c8cd17c
rafael <rafaelsevla@gmail.com>
2018-09-09 21:31:54 -0300

    created form for messages

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourse
 
 def index(request):
 	courses = Course.objects.all()
@@ -20,8 +21,13 @@ def index(request):
 
 def details(request, slug):
 	courses = get_object_or_404(Course, slug=slug)
+	if request.method == 'POST':
+	    form = ContactCourse(request.POST)
+	else:
+		form = ContactCourse()
 	context = {
-		'course': courses
+		'course': courses,
+		'form': form
 	}
 	template_name = 'courses/details.html'
 	return render(request, template_name, context)

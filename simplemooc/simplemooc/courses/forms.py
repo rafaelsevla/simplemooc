a40bb95c2c9022bbf23cfde3f96e3b2dceb6877c
rafael <rafaelsevla@gmail.com>
2018-09-09 23:14:18 -0300

    added html to send email

--- simplemooc/simplemooc/courses/forms.py
+++ simplemooc/simplemooc/courses/forms.py
@@ -2,18 +2,20 @@ from django import forms
 from django.core.mail import send_mail
 from django.conf import settings
 
-class ContactCourse(forms.Form):
-	name = forms.CharField(label='Nome', max_length=100)
-	email = forms.EmailField(label='E-mail')
-	message = forms.CharField(label='Mensagem/Dúvida', widget=forms.Textarea)
+from simplemooc.core.mail import send_mail_template
 
-	def send_mail(self, course):
-		subject = '[%s] Contato' % course
-		message = 'Nome: %(name)s;Email: %(email)s;%(message)s'
-		context = {
-			'name': self.cleaned_data['name'],
-			'email': self.cleaned_data['email'],
-			'message': self.cleaned_data['message']
-		}
-		message = message % context
-		send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [settings.CONTACT_EMAIL])
+class ContactCourse(forms.Form):
+    name = forms.CharField(label='Nome', max_length=100)
+    email = forms.EmailField(label='E-mail')
+    message = forms.CharField(label='Mensagem/Dúvida', widget=forms.Textarea)
+
+    def send_mail(self, course):
+        subject = '[%s] Contato' % course
+        message = 'Nome: %(name)s;Email: %(email)s;%(message)s'
+        context = {
+            'name': self.cleaned_data['name'],
+            'email': self.cleaned_data['email'],
+            'message': self.cleaned_data['message']
+        }
+        template_name = 'courses/contact_email.html'
+        send_mail_template(subject, template_name, context, [settings.CONTACT_EMAIL])

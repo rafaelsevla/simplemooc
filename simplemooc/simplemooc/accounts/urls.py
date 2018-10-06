fe54191222a814d18b81479d5791e87a46f1f856
rafael <rafaelsevla@gmail.com>
2018-10-06 13:46:58 -0300

    added form of cadaster

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+from simplemooc.accounts.views import register
+
 urlpatterns = [
-    url(r'^entrar/$', auth_views.LoginView.as_view(template_name='login.html'), name='login')
+    url(r'^entrar/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^cadastro/$', register, name='register')
 ]

5c3c585a947671011e1ec794476da649739ce7b4
rafael <rafaelsevla@gmail.com>
2018-10-06 15:44:45 -0300

    created dashboard to user and access only auth

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
-from simplemooc.accounts.views import register
+from simplemooc.accounts.views import register, dashboard
 
 urlpatterns = [
+    url(r'^$', dashboard, name='dashboard'),
     url(r'^entrar/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^sair/$', auth_views.LogoutView.as_view(next_page='core:home'), name='logout'),
-    url(r'^cadastro/$', register, name='register')
+    url(r'^cadastro/$', register, name='register'),
 ]

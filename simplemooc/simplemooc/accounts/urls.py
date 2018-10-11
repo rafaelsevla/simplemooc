245dc8c0b6288a581aaac0c2cdf6bf27fdec9b0c
rafael <rafaelsevla@gmail.com>
2018-10-11 10:00:12 -0300

    added form to edit user and password and fix dashboard and css

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
-from simplemooc.accounts.views import register, dashboard
+from simplemooc.accounts.views import register, dashboard, edit, edit_password
 
 urlpatterns = [
     url(r'^$', dashboard, name='dashboard'),
     url(r'^entrar/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^sair/$', auth_views.LogoutView.as_view(next_page='core:home'), name='logout'),
-    url(r'^cadastro/$', register, name='register'),
+    url(r'^cadastre-se/$', register, name='register'),
+    url(r'^editar/$', edit, name='edit'),
+    url(r'^editar-senha/$', edit_password, name='edit_password'),
 ]
--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
 
 
 @login_required
@@ -19,7 +19,7 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user = authenticate(username = user.username, password=form.cleaned_data['password1'])
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
             login(request, user)
             return redirect('core:home')
     else:
@@ -27,4 +27,35 @@ def register(request):
     context= {
         'form': form
     }
+    return render(request, template_name, context)
+
+
+@login_required()
+def edit(request):
+    template_name = 'edit.html'
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountForm(instance=request.user)
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+@login_required
+def edit_password(request):
+    template_name = 'edit_password.html'
+    context = {}
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            context['success'] = True
+    else:
+        form = PasswordChangeForm(user=request.user)
+    context['form'] = form
     return render(request, template_name, context)

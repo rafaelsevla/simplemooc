cb97b8c392aeb34a0cb7a807c25f9d522db27e82
rafael <rafaelsevla@gmail.com>
2018-10-28 12:22:33 -0300

    options in courses added: announcements to courses and option of undo enrollment this

--- simplemooc/simplemooc/courses/urls.py
+++ simplemooc/simplemooc/courses/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url, include
-from simplemooc.courses.views import index, details, enrollment
+from simplemooc.courses.views import (index,
+                                      details,
+                                      enrollment,
+                                      announcements,
+                                      undo_enrollment)
 # from simplemooc.courses.views import home, contact
 
 urlpatterns = [
@@ -7,4 +11,6 @@ urlpatterns = [
     #url(r'^(?P<pk>\d+)/$', details, name='details'),
     url(r'^(?P<slug>[\w_-]+)/$', details, name='details'),
     url(r'^(?P<slug>[\w_-]+)/inscricao/$', enrollment, name='enrollment'),
+    url(r'^(?P<slug>[\w_-]+)/cancelar-inscricao/$', undo_enrollment, name='undo_enrollment'),
+    url(r'^(?P<slug>[\w_-]+)/anuncios/$', announcements, name='announcements'),
 ]
--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -14,14 +14,6 @@ def index(request):
     }
     return render(request, template_name, context)
 
-# def details(request, pk):
-# 	courses = get_object_or_404(Course, pk=pk)
-# 	context = {
-# 		'course': courses
-# 	}
-# 	template_name = 'courses/details.html'
-# 	return render(request, template_name, context)
-
 
 def details(request, slug):
     courses = get_object_or_404(Course, slug=slug)
@@ -50,6 +42,41 @@ def enrollment(request, slug):
         # enrollment.active()
         messages.success(request, 'Cadastro feito com sucesso!')
     else:
-        messages.warning(request, 'Você já está cadastrado nesse curso!!', extra_tags='alert')
+        messages.warning(request, 'Você já está cadastrado nesse curso!!')
 
     return redirect('accounts:dashboard')
+
+
+@login_required
+def undo_enrollment(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    enrollment = get_object_or_404(
+        Enrollment, user=request.user, course=course
+    )
+    if request.method == 'POST':
+        enrollment.delete()
+        messages.success(request, 'Sua inscrição foi cancelada com sucesso')
+        return redirect('accounts:dashboard')
+    template = 'courses/undo_enrollment.html'
+    context = {
+        'enrollment': enrollment,
+        'course': course
+    }
+    return render(request, template, context)
+
+
+@login_required
+def announcements(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    if not request.user.is_staff:
+        enrollment = get_object_or_404(
+            Enrollment, user=request.user, course=course
+        )
+        if not enrollment.is_approved():
+            messages.error(request, 'A sua inscrição está pendente')
+            return redirect('account:dashboard')
+    template = 'courses/announcements.html'
+    context = {
+        'course': course
+    }
+    return render(request, template, context)

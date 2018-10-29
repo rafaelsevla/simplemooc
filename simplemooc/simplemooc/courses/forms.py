bd675799b404d0a55d5414a82667e9b5fcc9dd44
rafael <rafaelsevla@gmail.com>
2018-10-28 21:16:58 -0300

    add courses functions of courses: see video and download this just access course if is enrollment in this

--- simplemooc/simplemooc/courses/forms.py
+++ simplemooc/simplemooc/courses/forms.py
@@ -3,6 +3,8 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 from simplemooc.core.mail import send_mail_template
+from .models import Comment
+
 
 class ContactCourse(forms.Form):
     name = forms.CharField(label='Nome', max_length=100)
@@ -19,3 +21,10 @@ class ContactCourse(forms.Form):
         }
         template_name = 'courses/contact_email.html'
         send_mail_template(subject, template_name, context, [settings.CONTACT_EMAIL])
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['comment']

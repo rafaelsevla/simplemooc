29c2ac0fa18c0a6db3aaa392464e5f461c5d4f7a
rafael <rafaelsevla@gmail.com>
2018-11-03 00:02:42 -0300

    finish page of forum

--- simplemooc/simplemooc/forum/models.py
+++ simplemooc/simplemooc/forum/models.py
@@ -48,3 +48,23 @@ class Reply(models.Model):
         verbose_name = 'Resposta'
         verbose_name_plural = 'Respostas'
         ordering = ['-correct', 'created']
+
+
+def post_save_reply(created, instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+    if instance.correct:
+        instance.thread.replies.exclude(pk=instance.pk).update(correct=False)
+
+
+def post_delete_reply(instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+
+
+models.signals.post_save.connect(
+    post_save_reply, sender=Reply, dispatch_uid='post_save_reply'
+)
+models.signals.post_delete.connect(
+    post_delete_reply, sender=Reply, dispatch_uid='post_delete_reply'
+)
